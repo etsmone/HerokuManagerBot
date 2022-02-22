a77e0a60a492918321c00735aec96cfdc43381b6
ETS M1 Version <100184720+etsmone@users.noreply.github.com>
2022-02-22 05:45:12 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN","")
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY","")
-    AUTHORIZED_USERS = [int(user) for user in os.environ.get("AUTHORIZED_USERS").split(" ")]
+    BOT_TOKEN = os.environ.get("5148891104:AAGVQQjlidRf57Rr1khrRnB2_lPXzWs3cUY","")
+    HEROKU_API_KEY = os.environ.get("d502dd68-3121-40a1-b5e2-0b5d28e56efa","")
+    AUTHORIZED_USERS = [int(user) for user in os.environ.get("1380112160").split(" ")]
     TG_CHARACTER_LIMIT = 4000 
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME","")
+    HEROKU_APP_NAME = os.environ.get("ets-m1-161","")
